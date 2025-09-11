9bbc2c9218e38bb386c2371e31ad8ede56cdbcb2
Francesca <francesca.crucinio@warwick.ac.uk>
2025-09-11 18:33:26 +0200

    mixture

--- utils.py
+++ utils.py
@@ -63,3 +63,42 @@ def diagnostics(x, Niter, prob_val, true_sample, xx, yy, w = None):## only 1d fo
     return diag
     
     
+def diagnosticsHD(x, Niter, prob_val, true_sample, true_mean, true_cov, xx, yy, dx, w = None):## only 1d for now
+    d = x.shape[1]
+    diag = np.zeros((Niter, 6))
+    if (w is None):
+        for j in range(Niter):
+            # mean
+            diag[j, 0] = np.sum((np.mean(x[j, :], axis = 1) - true_mean)**2)
+            # variance
+            diag[j, 1] = np.sum((np.cov(x[j, :]) - true_cov)**2)
+            # prob
+#             diag[j, 2] = np.mean(x[j, :]>prob_val, axis = 0)
+#             tmp = 0
+#             for i in range(d):
+#                 # w1
+#                 tmp += stats.wasserstein_distance(x[j, i, :].flatten(), true_sample[i, :].flatten())
+#             diag[j, 3] = tmp/d
+            # kl
+            kde = stats.gaussian_kde(x[j, :])
+            kde_eval = kde(xx)
+            diag[j, 4] = dx**d*np.sum(kde_eval*(np.log(kde_eval)-np.log(yy)))
+            # mmd
+            diag[j, 5] = mmd_rbf(true_sample, x[j, :].T)
+    else:
+        for j in range(Niter):
+            # mean
+            diag[j, 0] = np.sum((np.sum(x[j, :]*w[j, :].T, axis = 1) - true_mean)**2)
+#             # variance
+#             diag[j, 1] = np.sum(x[j, :]**2*w[j, :].T) - diag[j, 0]**2
+#             # prob
+#             diag[j, 2] = np.sum((x[j, :]>prob_val)*w[j, :].T)
+#             # w1
+#             diag[j, 3] = stats.wasserstein_distance(x[j, :].flatten(), true_sample.flatten(), u_weights = w[j, :])
+            # kl
+            kde = stats.gaussian_kde(x[j, :], weights = w[j, :])
+            kde_eval = kde(xx)
+            diag[j, 4] = dx**d*np.sum(kde_eval*(np.log(kde_eval)-np.log(yy)))
+            # mmd
+            diag[j, 5] = mmd_rbf(true_sample, x[j, :].T, w = w[j, :])
+    return diag
