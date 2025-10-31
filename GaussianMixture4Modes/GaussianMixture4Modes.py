2a802ef5b4342211d5cda064d0a36b8cb68051bb
Francesca <francesca.crucinio@warwick.ac.uk>
2025-10-31 15:10:25 +0100

    expe 1

--- GaussianMixture4Modes/GaussianMixture4Modes.py
+++ GaussianMixture4Modes/GaussianMixture4Modes.py
@@ -2,7 +2,9 @@ import numpy as np
 from scipy.stats import multivariate_normal
 from scipy import linalg, stats
 from particles import resampling as rs
+from scipy.spatial.distance import pdist, squareform
 from sklearn import metrics
+from scipy.spatial.distance import cdist
 
 ### utils
 def gradient_4modes(x, ms, Sigmas, weights):
@@ -13,7 +15,7 @@ def gradient_4modes(x, ms, Sigmas, weights):
     denominator = np.zeros((weights.size, x.shape[1]))
     for j in range(weights.size):
         denominator[j, :] = weights[j]*multivariate_normal.pdf(x.T, ms[j,:], Sigmas[j,:,:])
-        gradient += -denominator[j, :]*np.matmul(linalg.inv(Sigmas[j,:,:]),(v.T-ms[j,:]).T)
+        gradient += -denominator[j, :]*np.matmul(linalg.inv(Sigmas[j,:,:]),(x.T-ms[j,:]).T)
     return gradient/np.sum(denominator, axis = 0)
 def logpi_4modes(x, ms, Sigmas, weights):
 #     v = x.view(float)
@@ -73,7 +75,8 @@ def BDL_kernelisedPDE(gamma, Niter, ms, Sigmas, weights, N, h, X0):
         gradient = gradient_4modes(X[i-1, :, :], ms, Sigmas, weights)
         X[i, :, :] = X[i-1, :, :] + gamma*gradient + np.sqrt(2*gamma)*np.random.multivariate_normal(np.zeros(d), np.eye(d), size = N).T
         beta = -logpi_4modes(X[i, :, :], ms, Sigmas, weights)
-        kernel_rate = multivariate_normal.pdf(np.kron(X[i, :, :].T, np.ones((N, 1))) - np.tile(X[i, :, :], N).T, np.zeros(d), h*np.eye(d)).reshape(N, N)
+#         kernel_rate = multivariate_normal.pdf(np.kron(X[i, :, :].T, np.ones((N, 1))) - np.tile(X[i, :, :], N).T, np.zeros(d), h*np.eye(d)).reshape(N, N)
+        kernel_rate = metrics.pairwise.rbf_kernel(X[i, :, :].T, X[i, :, :].T, h)
         beta = beta + np.log(np.mean(kernel_rate, axis = 0))
         beta = beta - np.mean(beta)
         kill = (beta >= 0) * (np.random.uniform(size = N) < 1-np.exp(-beta*gamma))
@@ -95,7 +98,8 @@ def BDL_kernelisedKL(gamma, Niter, ms, Sigmas, weights, N, h, X0):
         gradient = gradient_4modes(X[i-1, :, :], ms, Sigmas, weights)
         X[i, :, :] = X[i-1, :, :] + gamma*gradient + np.sqrt(2*gamma)*np.random.multivariate_normal(np.zeros(d), np.eye(d), size = N).T
         beta = -logpi_4modes(X[i, :, :], ms, Sigmas, weights)
-        kernel_rate = multivariate_normal.pdf(np.kron(X[i, :, :].T, np.ones((N, 1))) - np.tile(X[i, :, :], N).T, np.zeros(d), h*np.eye(d)).reshape(N, N)
+#         kernel_rate = multivariate_normal.pdf(np.kron(X[i, :, :].T, np.ones((N, 1))) - np.tile(X[i, :, :], N).T, np.zeros(d), h*np.eye(d)).reshape(N, N)
+        kernel_rate = metrics.pairwise.rbf_kernel(X[i, :, :].T, X[i, :, :].T, h)
         beta = beta + np.log(np.mean(kernel_rate, axis = 0))
         beta = beta - np.mean(beta) - 1 + np.sum(kernel_rate/np.sum(kernel_rate, axis = 0), axis = 1)
         kill = (beta >= 0) * (np.random.uniform(size = N) < 1-np.exp(-beta*gamma))
@@ -125,7 +129,8 @@ def SMC_WFR(gamma, Niter, ms, Sigmas, weights, X0):
         # MCMC move
         gradient_step = X[n-1, :, :] + gamma*gradient_4modes(X[n-1, :, :], ms, Sigmas, weights)
         X[n, :, :] = gradient_step + np.sqrt(2*gamma)*np.random.normal(size = (d, N))
-        kde_matrix = multivariate_normal.pdf(np.kron(X[n, :, :].T, np.ones((N, 1))) - np.tile(gradient_step, N).T, mean = np.zeros(d), cov = 2*gamma*np.eye(d)).reshape(N, N)
+#         kde_matrix = multivariate_normal.pdf(np.kron(X[n, :, :].T, np.ones((N, 1))) - np.tile(gradient_step, N).T, mean = np.zeros(d), cov = 2*gamma*np.eye(d)).reshape(N, N)
+        kde_matrix = metrics.pairwise.rbf_kernel(X[n, :, :].T, gradient_step.T, 1/(4*gamma))
         weight_denominator = np.mean(kde_matrix, axis = 1)
         logW = (1-np.exp(-gamma))*(logpi_4modes(X[n, :, :], ms, Sigmas, weights)-np.log(weight_denominator))
         W[n, :] = rs.exp_and_normalise(logW)
